ac3f62b8a9e8973a3b840ef09c46ac04df0c9a91
Tainara <anatainarabat@gmail.com>
2025-03-12 16:55:07 -0300

    Initial Commit

---
+++ FirstProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirstProject
+//
+//  Created by found on 12/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Oiiii, Tudo bom?!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirstProject/FirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectApp.swift
+//  FirstProject
+//
+//  Created by found on 12/03/25.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
