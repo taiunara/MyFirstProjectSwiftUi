3b4a02405a3f9bf46e4a9d62e50e6838cd4e833e
Tainara <anatainarabat@gmail.com>
2025-03-13 16:01:49 -0300

    Day two

---
+++ FirstProject/AddTaskView.swift
@@ -0,0 +1,18 @@
+//
+//  AddTaskView.swift
+//  FirstProject
+//
+//  Created by found on 13/03/25.
+//
+
+import SwiftUI
+
+struct AddTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddTaskView()
+}
---
+++ FirstProject/ListTaskView.swift
@@ -0,0 +1,18 @@
+//
+//  ListTaskView.swift
+//  FirstProject
+//
+//  Created by found on 13/03/25.
+//
+
+import SwiftUI
+
+struct ListTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListTaskView()
+}
